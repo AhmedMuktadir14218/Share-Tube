2f36e2b53ec64ddc318c03154f0f7ddc3aaef665
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-04-23 14:42:40 +0600

    app.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@
 // import { DB_NAME } from "./constance.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 }) 
-
+const port = process.env.PORT || 8000;
 connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`server is running at ${port}`)
+
+    })
+   
+})
+
+.catch((error)=>{
+    console.log("mongo db connection Failed",error)
+})
 // const app = express();
  
 //  ;(async ()=>{
